547efa19cbcf64a2c0949ab3133ac290399e254a
DoubleJK <kmeiguitar96@gmail.com>
2023-11-28 20:14:25 +0900

    오버워치 캐릭터 선택창

---
+++ js/overwatch.js
@@ -0,0 +1,19 @@
+console.log("Overwatch JS");
+
+document.addEventListener('DOMContentLoaded', function () 
+{
+  const container = document.querySelector('.container .heros');
+  for (let i = 1; i <= 32; ++i) 
+  {
+    const hero = document.createElement('div');
+    hero.classList.add('hero');
+
+    const image = document.createElement('div');
+    image.classList.add('image');
+
+    image.style.backgroundImage = `url("/image/overwatch/hero${i}.png")`;
+
+    hero.appendChild(image);
+    container.appendChild(hero);
+  }
+});
